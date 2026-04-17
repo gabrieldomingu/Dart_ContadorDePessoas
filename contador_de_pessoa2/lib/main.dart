5180d456953259242b803d85afcfc17609f5cd91
gabrieldomingu <gabrieldmg161009@gamil.com>
2026-04-16 21:56:51 -0300

    Add imagem oculta

--- contador_de_pessoa2/lib/main.dart
+++ contador_de_pessoa2/lib/main.dart
@@ -18,66 +18,86 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  void decrement(){print('decrement');}
-  void increment(){print('decrement');}
+  void decrement() {
+    print('decrement');
+  }
+
+  void increment() {
+    print('decrement');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Pode entrar',
-          style: TextStyle(
-            fontSize: 26,
-            color:  Color.fromARGB(255, 4, 87, 154),
-            fontWeight: FontWeight.w700
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/Imagens/image.jfif'),
+            fit: BoxFit.cover,
           ),
-          ),
-          Text('0',
-          style: TextStyle(
-            fontSize: 26,
-            color:  Color.fromARGB(255, 4, 87, 154),
-            fontWeight: FontWeight.w700
-          ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(onPressed: decrement,
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
-                fixedSize: const Size(100, 100),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(24)
-                ),
-              ),
-              
-               child: Text('Sair',
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Pode entrar!',
               style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-               ),
+                  fontSize: 50,
+                  color: Color.fromARGB(255, 4, 87, 154),
+                  fontWeight: FontWeight.w700),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(24),
+              child: Text(
+                '0',
+                style: TextStyle(
+                    fontSize: 100,
+                    color: Color.fromARGB(255, 4, 87, 154),
+                    fontWeight: FontWeight.w700),
               ),
-              ),
-              TextButton(onPressed: increment,
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.blue,
-                fixedSize: const Size(100, 100),
-                shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(24)
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24)),
+                  ),
+                  child: const Text(
+                    'Sair',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
                 ),
-              ),
-               child: Text('Entrar',
-               style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
+                const SizedBox(
+                  width: 32,
                 ),
-              ),
-              ),
-            ],
-          ),
-        ],
+                TextButton(
+                  onPressed: increment,
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    fixedSize: const Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24)),
+                  ),
+                  child: const Text(
+                    'Entrar',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
